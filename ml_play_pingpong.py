f75a6712afdbb2ede8c74631882145522b8fd925
skullhan0727 <38278584+skullhan0727@users.noreply.github.com>
2020-05-12 20:54:27 +0800

    Add files via upload

--- ml_play_pingpong.py
+++ ml_play_pingpong.py
@@ -4,6 +4,9 @@ The template of the script for the machine learning process in game pingpong
 
 # Import the necessary modules and classes
 from mlgame.communication import ml as comm
+import pandas as pd
+from os import path  
+import pickle
 
 def ml_loop(side: str):
     """
@@ -22,92 +25,26 @@ def ml_loop(side: str):
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here
     ball_served = False
+
+    filename = path.join(path.dirname(__file__),"save/clf.SVR.pickle")  #讀這隻檔案的位置
+    with open(filename, 'rb') as file:
+        clf = pickle.load(file)
+
     def move_to(player, pred) : #move platform to predicted position to catch ball 
         if player == '1P':
-            if scene_info["platform_1P"][0]+20  > (pred-10) and scene_info["platform_1P"][0]+20 < (pred+10): return 0 # NONE
-            elif scene_info["platform_1P"][0]+20 <= (pred-10) : return 1 # goes right
+            if scene_info["platform_1P"][0]+20  > (pred-20) and scene_info["platform_1P"][0]+20 < (pred+20): return 0 # NONE
+            elif scene_info["platform_1P"][0]+20 <= (pred-20) : return 1 # goes right
             else : return 2 # goes left
         else :
-            if scene_info["platform_2P"][0]+20  > (pred-10) and scene_info["platform_2P"][0]+20 < (pred+10): return 0 # NONE
-            elif scene_info["platform_2P"][0]+20 <= (pred-10) : return 1 # goes right
+            if scene_info["platform_2P"][0]+20  > (pred-20) and scene_info["platform_2P"][0]+20 < (pred+20): return 0 # NONE
+            elif scene_info["platform_2P"][0]+20 <= (pred-20) : return 1 # goes right
             else : return 2 # goes left
 
     def ml_loop_for_1P(): 
 
-        """        
-        if scene_info["ball_speed"][1] > 0 : # 球正在向下 # ball goes down
-            x = ( scene_info["platform_1P"][1]-scene_info["ball"][1] ) // scene_info["ball_speed"][1] # 幾個frame以後會需要接  # x means how many frames before catch the ball
-            pred = scene_info["ball"][0]+(scene_info["ball_speed"][0]*x)  # 預測最終位置 # pred means predict ball landing site 
-            bound = pred // 200 # Determine if it is beyond the boundary
-            if (bound > 0): # pred > 200 # fix landing position
-                if (bound%2 == 0) : 
-                    pred = pred - bound*200                    
-                else :
-                    pred = 200 - (pred - 200*bound)
-            elif (bound < 0) : # pred < 0
-                if (bound%2 ==1) :
-                    pred = abs(pred - (bound+1) *200)
-                else :
-                    pred = pred + (abs(bound)*200)
-            return move_to(player = '1P',pred = pred)
-        else : # 球正在向上 # ball goes up
-            return move_to(player = '1P',pred = 100)
-        """
- 
-        ball_x=scene_info["ball"][0]
-        ball_y=scene_info["ball"][1]
-
-        platform_x=scene_info["platform_1P"][0]
-        platform_y=scene_info["platform_1P"][1]
-
-        ball_speed_x=scene_info["ball_speed"][0]
-        ball_speed_y=vector_y_down=scene_info["ball_speed"][1]
-
-        now_ball_x=ball_x
-        now_ball_y=ball_y
-
-        vector_x_right= True if ball_speed_x>0 else False
-        vector_y_down= True if ball_speed_y >0 else False
-
-        hit_y=0
-
-        while hit_y<415:
-            if vector_x_right:
-                hit_x=195
-            else:
-                hit_x=0
-
-            if vector_y_down:
-                hit_y=415
-            else:
-                hit_y=80     
-
-            if abs(now_ball_x-hit_x) <abs(now_ball_y-hit_y):
-                if vector_y_down:
-                    hit_y=((abs(now_ball_x-hit_x)-1)//abs(ball_speed_x)+1)*abs(ball_speed_y)+now_ball_y
-                    if hit_y>415:
-                        hit_y=((abs(now_ball_x-hit_x))/abs(ball_speed_x))*abs(ball_speed_y)+now_ball_y
-                else:
-                    hit_y=-((abs(now_ball_x-hit_x)-1)//abs(ball_speed_x)+1)*abs(ball_speed_y)+now_ball_y
-                    if hit_y<0:
-                        hit_y=-((abs(now_ball_x-hit_x))/abs(ball_speed_x))*abs(ball_speed_y)+now_ball_y
-                        
-                vector_x_right=not vector_x_right 
-            else:
-                #這邊可能會切球
-                if vector_x_right:
-                    hit_x=((abs(now_ball_y-hit_y))/abs(ball_speed_x))*abs(ball_speed_x)+now_ball_x
-                else:
-                    hit_x=-((abs(now_ball_y-hit_y))/abs(ball_speed_y))*abs(ball_speed_x)+now_ball_x
-                vector_y_down=not vector_y_down  
-
-
-
-            now_ball_x=hit_x
-            now_ball_y=hit_y
-
-        should_x=hit_x
-        return move_to(player = '1P',pred = should_x)
+        y = clf.predict(feature)
+        
+        return move_to(player = '1P',pred = y)
     
 
     def ml_loop_for_2P():  # as same as 1P
@@ -137,6 +74,14 @@ def ml_loop(side: str):
         # 3.1. Receive the scene information sent from the game process
         scene_info = comm.recv_from_game()
 
+
+
+
+        
+        data = [[scene_info["ball"][0],scene_info["ball"][1],scene_info["ball_speed"][0],scene_info["ball_speed"][1],scene_info["blocker"][1]]]
+        feature = pd.DataFrame(data,columns=["ball_x","ball_y","ball_speed_x","ball_speed_y","blocker_x"])
+        print (feature)
+
         # 3.2. If either of two sides wins the game, do the updating or
         #      resetting stuff and inform the game process when the ml process
         #      is ready.
@@ -153,7 +98,7 @@ def ml_loop(side: str):
 
         # 3.4 Send the instruction for this frame to the game process
         if not ball_served:
-            comm.send_to_game({"frame": scene_info["frame"], "command": "SERVE_TO_RIGHT"})
+            comm.send_to_game({"frame": scene_info["frame"], "command": "SERVE_TO_LEFT"})
             ball_served = True
         else:
             if side == "1P":
@@ -175,4 +120,8 @@ def ml_loop(side: str):
             #if scene_info["ball"][1]==80 or scene_info["ball"][1]==415:
             if scene_info["ball"][1]==415:
                 print("******x:",scene_info["ball"][0],"y:",scene_info["ball"][1],"PLATFORM_x:",scene_info["platform_1P"][0],"PLATFORM_y:",scene_info["platform_1P"][1])
-            print("x:",scene_info["ball"][0],"y:",scene_info["ball"][1],"PLATFORM_x:",scene_info["platform_1P"][0],"PLATFORM_y:",scene_info["platform_1P"][1])
+            #print("x:",scene_info["ball"][0],"y:",scene_info["ball"][1],"ball_speed_x:",scene_info["ball_speed"][0],"ball_speed_y:",scene_info["ball_speed"][1],"PLATFORM_x:",scene_info["platform_1P"][0],"PLATFORM_y:",scene_info["platform_1P"][1])
+            #print("block_x:",scene_info["blocker"][0],"block_y:",scene_info["blocker"][1])
+            print("x:",scene_info["ball"][0],"y:",scene_info["ball"][1],"ball_speed_x:","block_x:",scene_info["blocker"][0],"block_y:",scene_info["blocker"][1],scene_info["ball_speed"][0],"ball_speed_y:",scene_info["ball_speed"][1],"PLATFORM_x:",scene_info["platform_1P"][0],"PLATFORM_y:",scene_info["platform_1P"][1])
+
+
